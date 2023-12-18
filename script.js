83fbb303e82c0f40f10ddb88dde77c641a90073b
Cody <1027166@student.sae.edu.au>
2023-12-19 02:27:30 +0800

    Colours, fonts, sticky header, mobile movement

--- script.js
+++ script.js
@@ -17,3 +17,25 @@ $(document).ready(function () {
         $('.carousel').removeClass('color-1 color-2 color-3').addClass('color-' + colorIndex);
     });
 });
+
+
+
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () { myFunction() };
+
+// Get the header
+var header = document.getElementById("stickyHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
